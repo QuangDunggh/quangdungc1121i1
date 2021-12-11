d74c615254cf0c997a5e69c0f0b3cb82186d2641
quangdunggh <dphanquang@gmail.com>
2021-12-11 09:26:55 +0700

    update file

---
+++ BaitapHTML&JS/BaitapJS/thuchanhIfElse/example.js
@@ -0,0 +1,12 @@
+var a = 7.6;
+if (a > 9) 
+    console.log('xuat sac')
+
+if (9 > a && a >= 8) 
+    console.log('gioi')
+
+if (8 > a && a >= 7) 
+    console.log('kha')
+
+if (7 > a && a >= 5) 
+    console.log('trung binh')
