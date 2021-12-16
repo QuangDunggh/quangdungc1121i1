24b39aea804846e13368940fac42c1b0675f7d5e
quangdunggh <dphanquang@gmail.com>
2021-12-16 20:10:40 +0700

    update file

---
+++ BaitapHTML&JS/BaitapJS/thuchanhvonglap/site.js
@@ -0,0 +1,36 @@
+
+// let i = 5;
+// let count = 0;
+// while (i <= 100) {
+//     console.log(i);
+//     i += 5;
+//     count++
+// }
+// console.log(count)
+
+// let i = 5;
+// let count = 1;
+// do {
+//     console.log(i);
+//     i += 5;
+//     count++;
+// } while(i < 100);
+// console.log(count)
+
+
+
+
+// const a = 1 && NaN || 'Hi' && null && 100
+// const b = 1 || NaN && 'Hi' || null && 100
+// console.log(a)
+// console.log(a)
+// console.log(b)
+
+// let a = [1,3,5];
+// let b = [2,4,6];
+// a[2] = b[a.length - 3];
+// // console.log(a);
+var array = [Math.random(10)];
+for (i in array) {
+    console.log(array[i++])
+}
