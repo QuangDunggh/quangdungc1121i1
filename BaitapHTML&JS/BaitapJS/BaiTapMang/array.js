2e21150f26c34310e62b3a7e4e2b923d0e93f950
quangdunggh <dphanquang@gmail.com>
2021-12-20 19:39:06 +0700

    update file

--- BaitapHTML&JS/BaitapJS/BaiTapMang/array.js
+++ BaitapHTML&JS/BaitapJS/BaiTapMang/array.js
@@ -1,5 +1,7 @@
-let arr = [9,9,0,6,7,8,1,2,3,4,0];
-// arr.splice(11,1,99);
-arr.join('\n')
+// let arr = [9,9,0,6,7,8,1,2,3,4,0];
+// // arr.splice(11,1,99);
+// arr.join('\n')
 
-console.log(arr.join('+'));
+// console.log(arr.join('+'));
+let arr = [];
+console.log(arr.length)
--- BaitapHTML&JS/BaitapJS/site.js
+++
@@ -1,4 +0,0 @@
-let age = 18;
-console.log(age);
-let x = 5 + 7 + '4' + 2 + 3;
-console.log(x);
